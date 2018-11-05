270a93aa599c208f82074f4e600434c03fc18125
tinmaker <429398056@qq.com>
2018-11-05 08:08:54 +0800

    serial test gui test

---
+++ test/guitest.py
@@ -0,0 +1,7 @@
+import Tkinter
+
+top = Tkinter.Tk()
+top.mainloop()
+
+if __name__ == '__main__':
+	main()
---
+++ test/serialtest.py
@@ -0,0 +1,11 @@
+import serial
+import serial.tools.list_ports as lists
+import time
+
+
+def main():
+	print "aaa"
+	time.sleep(1)
+
+if __name__ == '__main__':
+	main()
